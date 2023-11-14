52051aca28036f076f1c63a59c3578b399dd0088
ulasylmz <150842309+ulasylmz@users.noreply.github.com>
2023-11-14 21:13:45 +0300

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+	int i;
+	
+	for(i=7;i<31;i+=7){
+		
+		printf("P.TESI %d\n",i);
+	}
+	for(i=1;i<31;i+=7){
+		
+		printf("SALI %d\n",i);
+	}
+	for(i=2;i<31;i+=7){
+		
+		printf("CARSAMBA %d\n",i);
+	}
+	for(i=3;i<31;i+=7){
+		
+		printf("PERSEMBE %d\n",i);
+	}
+	for(i=4;i<31;i+=7){
+		
+		printf("CUMA %d\n",i);
+	}
+	for(i=5;i<31;i+=7){
+		
+		printf("C.TESI %d\n",i);
+	}
+	for(i=6;i<31;i+=7){
+		
+		printf("PAZAR %d\n",i);
+	}
+	
+	
+	
+}
